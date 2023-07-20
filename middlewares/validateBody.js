16b7ba8a9aa7f42954d47f2c6f4ac1385141c6c2
tTanya98 <chichkinat98@gmail.com>
2023-07-20 20:08:55 +0300

    updt

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -2,14 +2,18 @@ const { HttpErrors } = require('../helpers');
 
 const validateBody = (contactSchema) => {
   const func = (req, res, next) => {
-    const { error } = contactSchema.validate(req.body); 
-  
-  if (error) {
-      throw (HttpErrors(400, error.message))
-  }
-  next();
-};
-return func;
+    const { error } = contactSchema.validate(req.body, { abortEarly: false });
+        const emptyField = !Object.keys(req.body).length;
+         
+        if (emptyField) {
+          throw HttpErrors(400, "missing fields")
+        }
+        if (error) {
+          throw (HttpErrors(400, error.message))
+        }
+        next();
+    };
+    return func;
 }
 
 module.exports = validateBody;
