cc60f33b3f58f4e92695c277960793a7e79d3334
tTanya98 <chichkinat98@gmail.com>
2023-08-02 20:41:30 +0300

    fixed errors

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -5,7 +5,7 @@ const getAllContacts = async (req, res) => {
   const { _id: owner } = req.user;
   const { page = 1, limit = 10, favorite = true} = req.query;
   const skip = (page - 1) * limit;
-      const result = await Contact.find({ owner }, "", { skip, limit: Number(limit), favorite })
+      const result = await contactsService.find({ owner }, "", { skip, limit: Number(limit), favorite })
       .populate("owner", "email"); 
   res.json(result); 
 };
